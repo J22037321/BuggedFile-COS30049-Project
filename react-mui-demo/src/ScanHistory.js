562bcb213c76fbd47021136b142b46a351854e3f
KokZhongBo <106200220@student.swin.edu.au>
2026-06-25 19:38:13 +0800

    Update Frontend

--- react-mui-demo/src/ScanHistory.js
+++ react-mui-demo/src/ScanHistory.js
@@ -1,5 +1,4 @@
 import recent from './image/recent.svg';
-<<<<<<< HEAD
 import correct from './image/correct.svg';
 import error from './image/error.svg';
 
@@ -20,15 +19,10 @@ export default function ScanHistory() {
   } 
 
   else {
-=======
-
-export default function ScanHistory() {
->>>>>>> fdf5c3800b20ab62096ba1b82599bf3f944e7d00
   return (
     <div className="App-card App-recent-scans">
         <h2>Recent Scans</h2>
         <div className="App-recent-scans-content">
-<<<<<<< HEAD
             <img src={error} alt="Correct" aria-hidden="true" />
             <div className="App-recent-scans-item">
                 <p>Assignment 1.pdf</p>
@@ -53,15 +47,4 @@ export default function ScanHistory() {
     </div>
   );
   }
-=======
-            <img src={recent} alt="Recent" aria-hidden="true" />
-            <span className="App-recent-scans-empty">No scans yet</span>
-        </div>
-    </div>
-  );
-
-//   return (
-
-//   )
->>>>>>> fdf5c3800b20ab62096ba1b82599bf3f944e7d00
 }
